d38788f25d0e1312948333d89466476aedaaddf4
virtualformacion <virtualformacion47@gmail.com>
2026-01-16 03:35:23 +0000

    Actualizar usuarios (create) desde admin

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const USERS = [
     { username: "cliente8570", password: "23143", expiresAt: new Date("2026-02-28"), emails: ["villadiegokaren38+promocion@gmail.com","villadiegokaren38+parades@gmail.com"] },
     { username: "cliente845", password: "11223344", expiresAt: new Date("2026-02-28"), emails: ["josgalaxy02+laotra@gmail.com"] },
     { username: "cliente886", password: "223344", expiresAt: new Date("2026-02-28"), emails: ["juancaww1929+88@gmail.com","garantiadisney@streamingperez.xyz"] },
-    { username: "cliente483", password: "38923", expiresAt: new Date("2026-02-25"), emails: ["josgalaxy02+goa@gmail.com"] }
+    { username: "cliente483", password: "38923", expiresAt: new Date("2026-02-25"), emails: ["josgalaxy02+goa@gmail.com"] },
+    { username: "cliente799", password: "34217", expiresAt: new Date("2026-02-28"), emails: ["josgalaxy02+99@gmail.com","josedadowke+3@gmail.com","josgalaxy02+julio@gmail.com"] }
 ];
 
 const MAX_ATTEMPTS = 1000000;
